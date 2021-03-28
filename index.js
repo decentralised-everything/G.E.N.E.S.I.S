56ef6cdc67c4f8c5303abed7c16eec7246149119
avgCoderr <f20201677@pilani.bits-pilani.ac.in>
2021-03-28 09:39:14 +0000

    configured producshon

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const isDevelopment = process.env.ENV === "development";
 
 const REDIS_URL = isDevelopment
   ? "redis://127.0.0.1:6379"
-  : "redis://h:p05f9a274bd0e2414e52cb9516f8cbcead154d7d61502d32d9750180836a7cc05@ec2-34-225-229-4.compute-1.amazonaws.com:19289";
+  : "redis://:p1cedb41e24fca21ed276d480d96469ddfbd5762419ded5b33d8d90d80899e914@ec2-35-169-115-180.compute-1.amazonaws.com:27939";
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
